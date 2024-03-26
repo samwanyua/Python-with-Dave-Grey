9e2659e792138b9bb7a4ba37d847e3fe3cfe6b70
samwanyua <samexample8@gmail.com>
2024-03-26 16:58:37 +0300

    Impelementing lamda functions

--- lamda.py
+++ lamda.py
@@ -10,3 +10,12 @@ print(addTwo(322))
 sum = lambda a, b : a * b
 print(sum(32, 29))
 
+##################################################
+def funcBUilder(x):
+    return lambda num : num + x
+
+addTen = funcBUilder(10) # x = 10
+
+addTwenty = funcBUilder(20) # x = 20
+
+print(addTwenty(2)) # num = 2
