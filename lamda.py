9d365c6daba78540a3275fafbfaca9c763ea9719
samwanyua <samexample8@gmail.com>
2024-03-26 16:50:09 +0300

    Impelementing lamda functions

---
+++ lamda.py
@@ -0,0 +1,12 @@
+# lamda functions
+squared = lambda num: num * num
+
+print(squared(2))
+
+addTwo = lambda num : num + 200
+print(addTwo(322))
+
+# with two parameters
+sum = lambda a, b : a * b
+print(sum(32, 29))
+
