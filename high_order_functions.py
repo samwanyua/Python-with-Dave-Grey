4f882cc73b37f933091435028e94f83b7b5b0176
samwanyua <samexample8@gmail.com>
2024-03-26 21:44:41 +0300

    Implementing High order functions and map() method

---
+++ high_order_functions.py
@@ -0,0 +1,21 @@
+# High Order Functions
+'''
+- Takes one or more functions as an argument
+- Returns a functions as its result
+'''
+
+def func_builder():
+    pass
+
+
+
+
+numbers = [3,7,12,18,21,20]
+
+squared_nums = map(lambda num : num * num, numbers)
+
+print(list(squared_nums))
+
+# for num in numbers:
+#     print(num)
+
