8366a49ca90230b975b48b184f4dd69f1dcfdd7e
samwanyua <samexample8@gmail.com>
2024-03-27 14:38:34 +0300

    Implementing commandline arguments

--- cli.py
+++ cli.py
@@ -1,15 +1,31 @@
-import argparse
+def hello(name, lang):
+    greetings = {
+        "English": "Hello",
+        "Spanish": "Hola",
+        "German": "Hallo",
+        "Swahili": "Jambo",
+    }
+    msf = f"{greetings[lang]} {name}"
+    print(msf)
 
-parser = argparse.ArgumentParser(
-    description="Provides personal greetings."
-)
+if __name__ == '__main__':
 
-parser.add_argument(
-    "-n", "--name", metavar="name", 
-    required=True, help="The name of the person to greet"
-)
+    import argparse
 
-args = parser.parse_args()
+    parser = argparse.ArgumentParser(
+        description="Provides personal greetings."
+    )
 
-msf = f"Hello {args.name}"
-print(msf)
+    parser.add_argument(
+        "-n", "--name", metavar="name", 
+        required=True, help="The name of the person to greet"
+    )
+
+    parser.add_argument(
+        "-l", "--lang", metavar="language",
+        required=True, choices=["English","Spanish","German", "Swahili" ],
+        help="The language of the greeting."
+    )
+    args = parser.parse_args()
+
+    hello(args.name, args.lang)
